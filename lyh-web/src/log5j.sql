362fc91d8b809150487e5419c7b69b3b23ae7f9c
baekjiyoung <bjy8440@nate.com>
2015-06-08 14:02:15 +0900

    sql 추가

---
+++ lyh-web/src/log5j.sql
@@ -0,0 +1,23 @@
+create table member(
+	member_no number primary key,
+	id varchar2(50) not null,
+	password varchar2(50) not null,
+	name varchar2(50) not null,
+	address varchar2(50) not null,
+	identity_no varchar2(50) not null,
+	mail_address varchar2(50) not null,
+	member_type varchar2(10) not null
+)
+drop table member
+
+
+create table board(
+	board_no number(1000) primary key,
+	board_type varchar2(10) not null,
+	title varchar2(500) not null,
+	writer varchar2(500) not null,
+	content varchar2() not null,
+	constraint fk_member foreign key(member_no) references member(member_no)
+)
+
+drop table board
