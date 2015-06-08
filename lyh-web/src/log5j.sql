b7e6d2d25d04c5f2f90aa2cf8ecc78ff9cbccece
ningoop <akdhkds@naver.com>
2015-06-08 15:33:27 +0900

    sql 생성은 가능하게 수정함. 세부사항 계속 수정해야함.

--- lyh-web/src/log5j.sql
+++ lyh-web/src/log5j.sql
@@ -11,19 +11,21 @@ create table member(
 )
 drop table member
 -- board
+drop table board
 create table board(
-	board_no number(1000) primary key,
+	board_no number primary key,
 	board_type varchar2(10) not null,
-	title varchar2(500) not null,
-	writer varchar2(500) not null,
-	content varchar2() not null,
-	constraint fk_member foreign key(member_no) references member(member_no)
+	title varchar2(50) not null,
+	writer varchar2(50) not null,
+	content clob not null,
+	member_no number not null
+	constraint fk_member_no_4 references member(member_no)
 )
 
 drop table board
 -- statistics
 create table statistics(
-	age number not null,
+	age number,
 	field varchar(50),
 	applicate_count number not null,
 	primary key(age,field)
@@ -33,7 +35,7 @@ drop table statistics
 
 --recruit
 create table recruit(
-	recruit_no primary key,
+	recruit_no number primary key,
 	title varchar2(50) not null,
 	field varchar2(100) not null,
 	area varchar2(100) not null,
@@ -44,17 +46,23 @@ create table recruit(
 	
 drop table recruit
 
+drop table scheduler
 create table scheduler(
-constraint fk_member_no references member(member_no),
 field varchar(50) not null,
 location varchar(50) not null,
 start_date date not null,
-end_date date not null
+end_date date not null,
+member_no number primary key
+constraint fk_member_no_5 references member(member_no)
 )
 
+drop table voluntary_service_applicate
 create table voluntary_service_applicate(
-constraint fk_recruit_no references recruit(recruit_no),
-constraint fk_member_no references member(member_no)
+recruit_no number not null,
+member_no number not null
+constraint fk_recruit_no references recruit(recruit_no)
+constraint fk_member_no_2 references member(member_no),
+primary key(recruit_no,member_no)
 )
 
 create table QnA_board(
@@ -64,33 +72,42 @@ content varchar2(50) not null,
 ref varchar2(50) not null,
 restep varchar2(50) not null,
 relevel varchar2(50) not null,
-constraint fk_member_no references member(member_no)
+member_no number not null
+constraint fk_member_no_3 references member(member_no)
 )
 --comment
-create table comment(
+drop table comment1
+create table ymv_comment(
 	comment_no number primary key,
 	writer varchar2(50) not null,
 	content varchar2(50) not null,
 	time_posted date not null,
+	board_no number not null
 	constraint fk_board_no references board(board_no)
 )
 
 --picture
 create table picture(
-	picture_no number(50) primary key,
+	picture_no number primary key,
 	picture_name varchar2(50) not null,
-	board_no number(50) not null
+	board_no number not null
 )
 
 -- voluntary_applicant_ok
 create table voluntary_applicant_ok(
-	constraint fk_applicate_recruit references voluntary_service_applicate(recruit_no),
-	constraint fk_applicate_member  references voluntary_service_applicate(member_no)
+	recruit_no number not null,
+	member_no number not null
+	constraint fk_recruit_no_1 references recruit(recruit_no)
+	constraint fk_member_no_1 references member(member_no),
+	primary key(recruit_no,member_no)
 )
 
 -- voluntary_applicant 확인해보것음
 create table voluntary_applicant(
-	constraint fk_applicate_recruit references voluntary_service_applicate(recruit_no),
-	constraint fk_applicate_member  references voluntary_service_applicate(member_no)
+	recruit_no number not null,
+	member_no number not null
+	constraint fk_recruit_no_2 references recruit(recruit_no)
+	constraint fk_member_no_2 references member(member_no),
+	primary key(recruit_no,member_no)
 )
 
