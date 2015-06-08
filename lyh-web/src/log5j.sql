5b1a7b380caec2da8c8bcc6b1a6bff717b998368
ningoop <akdhkds@naver.com>
2015-06-08 13:51:33 +0900

    sql member table 작성

---
+++ lyh-web/src/log5j.sql
@@ -0,0 +1,11 @@
+create table member(
+	member_no number primary key,
+	id varchar2(50) not null,
+	password varchar2(50) not null,
+	name varchar2(50) not null,
+	address varchar2(50) not null,
+	identity_no varchar2(50) not null,
+	mail_address varchar2(50) not null,
+	member_type number not null
+)
+drop table member
